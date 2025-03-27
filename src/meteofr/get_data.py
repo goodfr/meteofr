4ec2c4303834c11d5f54c1e15a4d5380f2db2dee
goodfr <44895259+goodfr@users.noreply.github.com>
2025-03-27 11:49:09 +0100

    🚧 wip, get_all_ref ok + cahcing

--- src/meteofr/get_data.py
+++ src/meteofr/get_data.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import requests
 import numba
+from functools import cache
+import logging
 
 # ref: https://portail-api.meteofrance.fr/web/fr/api/test/a5935def-80ae-4e7e-83bc-3ef622f0438d/fe8c79d6-dcae-46f7-9e1f-6d5a8be4c3b8
 
@@ -18,6 +20,14 @@ APPLICATION_ID = (
 
 # url to obtain acces token
 TOKEN_URL = "https://portail-api.meteofrance.fr/token"
+
+list_dep = [str(i) for i in range(1, 96)]
+list_dep.extend(
+    ["971", "972", "973", "974", "975", "984", "985", "986", "987", "988", "99"]
+)
+url_api = "https://public-api.meteofrance.fr/public/DPClim/v1"
+
+logger = logging.getLogger("meteofr")
 
 
 class Client(object):
@@ -64,7 +74,8 @@ class Client(object):
         self.session.headers.update({"Authorization": "Bearer %s" % token})
 
 
-def get_ref(dep: int, prm: str) -> pd.DataFrame:
+@cache
+def get_ref(dep: str, prm: str) -> pd.DataFrame:
     """To get data from API into a DataFrame result
 
     Args:
@@ -75,12 +86,12 @@ def get_ref(dep: int, prm: str) -> pd.DataFrame:
         pd.DataFrame: result dataframe
     """
     from json import loads
-    
+
+    logger.debug(f"begin get ref: {dep}, {prm}")
     request_url = (
-        "https://public-api.meteofrance.fr/public/DPClim/v1/liste-stations/quotidienne?"
-        f"id-departement={dep}&parametre={prm}"
+        f"{url_api}/liste-stations/quotidienne?id-departement={dep}&parametre={prm}"
     )
-    
+
     client = Client()
 
     client.session.headers.update({"Accept": "application/json"})
@@ -91,20 +102,35 @@ def get_ref(dep: int, prm: str) -> pd.DataFrame:
     )
 
     df_ref_geo: pd.DataFrame = pd.json_normalize(loads(response.content))
+    
 
     return df_ref_geo
 
-def get_all_ref(list_dep: list[int]) -> pd.DataFrame:
-    # utiliser platformdirs pour la gestion de l'arbo du cache par OS ?
-    # pour éviter les redownload
+
+# use caching here ?
+@cache
+def get_all_ref(list_dep: list[str] = list_dep) -> pd.DataFrame:
+
+    from pathlib import Path
+    from os import makedirs, path
+    from tqdm import tqdm
+
+    logger.debug("begin get all ref")
+    dir_cache = Path.home().joinpath(".meteofr")
+
+    makedirs(dir_cache, exist_ok=True)
+
     df_list = []
-    # for i in range(1, 96):
-    for i in list_dep:
-        df_list.append(get_ref(dep = i, prm="temperature"))
+    for i in tqdm(list_dep[:10]):
+        df_list.append(get_ref(dep=i, prm="temperature"))
 
     df_ref_geo = pd.concat(df_list)
+    df_ref_geo.to_csv(path.join(dir_cache, "ref.csv"))
     
+    logger.debug("end get all ref")
+
     return df_ref_geo
+
 
 @numba.jit
 def hvs(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
@@ -118,6 +144,7 @@ def hvs(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
     d_km = 6367 * c
 
     return d_km
+
 
 @numba.jit
 def get_dist(vec: np.ndarray, ref: np.ndarray) -> np.ndarray:
@@ -133,6 +160,7 @@ def get_dist(vec: np.ndarray, ref: np.ndarray) -> np.ndarray:
 
     return res
 
+
 @numba.jit
 def get_closest_point(
     vec: np.ndarray, ref: np.ndarray
@@ -141,17 +169,48 @@ def get_closest_point(
 
     return mat.argmin(axis=1), mat.min(axis=1)
 
-def get_weather(point = None, id = None, name=None):
-    
-    assert not all([point is None, id is None, name is None]), "1 parameter point, id or name must be given"
-    
+
+def get_weather(point=None, id=None, name=None):
+    assert not all([point is None, id is None, name is None]), (
+        "1 parameter point, id or name must be given"
+    )
+
+    # get all stations
     df_ref_geo = get_all_ref()
 
-if __name__ == "__main__":
+    # find the closest
+    if id or name:
+        raise NotImplementedError
+    elif point is not None:
+        d, idx = get_closest_point(
+            vec=np.asarray(
+                [point],
+                dtype=(float, float),
+                ref=df_ref_geo.to_numpy(dtype=(float, float)),
+            )
+        )
+    # get weather data from closest station
+    # api call
 
+    # results
+    return
+
+
+if __name__ == "__main__":
     # on veut
-    # donner un site/coord/... + temporalité et récoupérer les infos requises
-    
+    # donner un site/coord/... + temporalité et récupérer les infos requises
+
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(ch)
+
     test_point = (45.932050, 2.000847)
-    
-    # get_weather()
+
+    get_weather(point=test_point)
